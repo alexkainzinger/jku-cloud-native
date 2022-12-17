5a64fca54a498410cd36ab7bd5e50f6011131d0d
alexkainzinger <alexander@kainzinger.dev>
2022-12-17 17:27:57 +0100

    feat(cluster): ingress & service for staging frontend

--- k3d-create-cluster.sh
+++ k3d-create-cluster.sh
@@ -8,8 +8,5 @@ kubectl create namespace argocd
 # install argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# expose argocd-server with a LoadBalancer
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
 # start the GitOps with creation of argo-cd app
 kubectl -n argocd apply -f ./argocd/argocd-app.yml
